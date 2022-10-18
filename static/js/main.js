22a19753dd646feb30332f3ce2726163c019c904
RATNAMPavidran <pavidran.ratnam@efrei.net>
2022-10-18 11:50:18 +0200

    modification html/css

--- static/js/main.js
+++ static/js/main.js
@@ -7,17 +7,7 @@ $(document).ready(function () {
     $('#confidence_1').hide();
 
     // Upload Preview
-    function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
-                $('#imagePreview').hide();
-                $('#imagePreview').fadeIn(650);
-            }
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
+   
     $("#imageUpload").change(function () {
         $('.image-section').show();
         $('#btn-predict').show();
@@ -50,7 +40,7 @@ $(document).ready(function () {
                 $('#result').fadeIn(600);
                 $('#result').text(' Result:  ' + data);
                 $('#confidence_0').fadeIn(600);
-                $('#confidence_0').text('Confidence of Pneumonia :   91.8' );
+                
 
                 console.log('Success!');
             },
