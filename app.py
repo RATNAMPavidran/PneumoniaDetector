3cbc11c1c133f1645b19be80e89f96cf1bc47b53
RATNAMPavidran <pavidran.ratnam@efrei.net>
2022-10-19 10:32:38 +0200

    modificaton de retour

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from __future__ import division, print_function
-# coding=utf-8
 import sys
 import os
 import glob
@@ -7,17 +6,17 @@ import re
 import numpy as np
 import PIL
 import tensorflow 
-# Keras
+
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from keras.models import load_model
 from keras.utils import image_utils
-# Flask utils
+
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 from flask import request
 
-# Define a flask app
+
 app = Flask(__name__)
 
 
@@ -25,7 +24,7 @@ app = Flask(__name__)
 def model_predict(img_path, model):
     img = image_utils.load_img(img_path, target_size=(224, 224))
 
-    # Preprocessing the image
+   
     x = image_utils.img_to_array(img)
     x = np.expand_dims(x, axis=0)
 
@@ -37,7 +36,7 @@ def model_predict(img_path, model):
 
 @app.route('/', methods=['GET'])
 def index():
-    # Main page
+    
     return render_template('index.html')
 
 model= load_model("model/model.h5")
@@ -47,23 +46,24 @@ cache={}
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
-        # Get the file from post request
+        
         f = request.files['file']
 
-        # Save the file to ./uploads
+       
         basepath = os.path.dirname(__file__)
         file_path = os.path.join(
             basepath, 'uploads', secure_filename(f.filename))
         f.save(file_path)
         print(cache)
-        # Make prediction
+        
         preds = model_predict(file_path,model)
         
         os.remove(file_path)
         
         cache['confidence_0'] = preds[0][0]
         cache['confidence_1'] = preds[0][1]
-        # Process your result for human
+        
+        
         pred_class = preds.argmax(axis=-1)         
         
         if pred_class == 1:
--- static/js/main.js
+++ static/js/main.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
             reader.readAsDataURL(input.files[0]);
         }
     }
-    
+
     $("#imageUpload").change(function () {
         $('.image-section').show();
         $('#btn-predict').show();
@@ -28,15 +28,15 @@ $(document).ready(function () {
         readURL(this);
     });
 
-    // Predict
+    
     $('#btn-predict').click(function () {
         var form_data = new FormData($('#upload-file')[0]);
 
-        // Show loading animation
+        
         $(this).hide();
         $('.loader').show();
 
-        // Make prediction by calling api /predict
+        
         $.ajax({
             type: 'POST',
             url: '/predict',
@@ -46,15 +46,11 @@ $(document).ready(function () {
             processData: false,
             async: true,
             success: function (data) {
-                // Get and display the result
-                //console.log(data);
                 $('.loader').hide();
                 $('#result').fadeIn(600);
                 $('#result').text(' Result:  ' + data);
                 $('#confidence_0').fadeIn(600);
                 
-
-                console.log('Success!');
             },
         });
     });
